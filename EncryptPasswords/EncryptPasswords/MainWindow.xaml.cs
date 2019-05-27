0e91bcc1f0dd507525e378d6633de8b2f25f5557
Olena Iliyshun <lenailyshun@gmail.com>
2019-05-27 20:31:08 +0300

    Create new progect Encrypt password

---
+++ EncryptPasswords/EncryptPasswords/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EncryptPasswords
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //encrypt password
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            txtLastPasswd.Text = GetMD5(txtPasswd.Text);
+        }
+        public string GetMD5(string text)
+        {
+            MD5CryptoServiceProvider mD5 = new MD5CryptoServiceProvider();
+            mD5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
+            byte[] result = mD5.Hash;
+            StringBuilder str = new StringBuilder();
+            for (int i = 1; i < result.Length; i++)
+            {
+                str.Append(result[i].ToString("x2"));
+            }
+            return str.ToString();
+        }
+    }
+}
